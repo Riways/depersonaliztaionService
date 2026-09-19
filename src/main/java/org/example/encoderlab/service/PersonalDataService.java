788b008857ebb0cc3b8f17c1e2e0fff780a42ef7
Riways <you@example.com>
2026-09-19 16:03:19 +0300

    Несколько тестов для extractEmails. Теперь сервис бросает ошибки при null в аргументе

--- src/main/java/org/example/encoderlab/service/PersonalDataService.java
+++ src/main/java/org/example/encoderlab/service/PersonalDataService.java
@@ -1,4 +1,50 @@
 package org.example.encoderlab.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
 public class PersonalDataService {
+    private static final Pattern EMAIL = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}");
+    private static final Pattern PHONE = Pattern.compile("(?<!\\d)(\\+375|80)([\\s\\-]?\\(?\\d{2}\\)?[\\s\\-]?\\d{3}[\\s\\-]?\\d{2}[\\s\\-]?\\d{2})(?!\\d)");
+
+    public List<String> extractEmails(String text) {
+        nonNullStringArgumentCheck(text, "Argument should not be NULL");
+        Matcher m = EMAIL.matcher(text);
+        List<String> result = new ArrayList<String>();
+        while (m.find())
+            result.add(m.group());
+        return result;
+    }
+
+    public List<String> extractPhones(String text) {
+        nonNullStringArgumentCheck(text, "Argument should not be NULL");
+        Matcher m = PHONE.matcher(text);
+        List<String> result = new ArrayList<String>();
+        while (m.find())
+            result.add(m.group());
+        return result;
+    }
+    public String removeEmails(String text){
+        nonNullStringArgumentCheck(text, "Argument should not be NULL");
+        return EMAIL.matcher(text).replaceAll("");
+    }
+
+    public String removePhones(String text){
+        nonNullStringArgumentCheck(text, "Argument should not be NULL");
+        return PHONE.matcher(text).replaceAll("");
+    }
+
+    public String removeAll(String text) {
+        return removePhones(removeEmails(text));
+    }
+
+    private void nonNullStringArgumentCheck(String arg, String msg){
+        if(arg == null)
+            throw new IllegalArgumentException(msg);
+    }
 }
--- src/test/java/org/example/encoderlab/EncoderlabApplicationTests.java
+++ src/test/java/org/example/encoderlab/EncoderlabApplicationTests.java
@@ -1,13 +1,46 @@
 package org.example.encoderlab;
 
+import org.example.encoderlab.service.PersonalDataService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class EncoderlabApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    private final PersonalDataService pdc = new PersonalDataService();
 
+    @Test
+    void extractEmails_shouldFindSingleEmail() {
+        String text = "Contact me at john.doe@example.com";
+        List<String> emails = pdc.extractEmails(text);
+        assertEquals(1, emails.size());
+        assertEquals("john.doe@example.com", emails.getFirst());
+    }
+    @Test
+    void extractEmails_shouldFindTwoEmails() {
+        String text = "Contact me at john.doe@example.com or john.doe2@example.com";
+        List<String> emails = pdc.extractEmails(text);
+        assertEquals(2, emails.size());
+        assertEquals("john.doe@example.com", emails.get(0));
+        assertEquals("john.doe2@example.com", emails.get(1));
+    }
+    @Test
+    void extractEmails_shouldFindTwoEqualEmailsAsOne() {
+        String text = "Contact me at john.doe@example.com or john.doe@example.com";
+        List<String> emails = pdc.extractEmails(text);
+        assertEquals(1, emails.size());
+        assertEquals("john.doe@example.com", emails.getFirst());
+    }
+    @Test
+    void extractEmails_shouldReturnEmptyListWhenNoEmails() {
+        String text = "Hello, world!";
+        List<String> emails = pdc.extractEmails(text);
+        assertTrue(emails.isEmpty());
+    }
+    @Test
+    void extractEmails_nullInputReturnsEmptyString(){
+        assertThrows(IllegalArgumentException.class, ()-> pdc.extractEmails(null));
+    }
 }
